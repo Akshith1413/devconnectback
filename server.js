f448d3fbc88a77ae0136dd731336a41e65a5a9f9
Akshith1413 <ravulaakshith1@gmail.com>
2025-12-02 19:06:34 +0530

    hi

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // ---------- MongoDB connect ----------
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => {
     console.error('Mongo connection error:', err);
